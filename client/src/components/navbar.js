8c3ed89373be354ecb6bd107c6f45c02dbe21a05
changemily <62681107+changemily@users.noreply.github.com>
2022-06-07 12:22:03 +1000

    added icons to navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,3 +1,7 @@
+/*
+In the navbar.js component, we will create a navigation bar that will link us to the required components using the following code.
+ */
+
 import React from "react";
  
 // We import bootstrap to make our application look better.
@@ -11,21 +15,18 @@ export default function Navbar() {
  return (
    <div>
      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <NavLink className="navbar-brand" to="/">
-       <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+       <NavLink className="navbar-home" to="/">
+       <img style={{"width" : 5 + '%'}} src={require('../icons/homeIcon.png')}></img>
        </NavLink>
-       <button
-         className="navbar-toggler"
-         type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
- 
+
+       <NavLink className="navbar-tracker" to="/">
+       <img style={{"width" : 10 + '%'}} src={require('../icons/trackerIcon.png')}></img>
+       </NavLink>
+
+       <NavLink className="navbar-watch" to="/">
+       <img style={{"width" : 20 + '%'}} src={require('../icons/watchListIcon.png')}></img>
+       </NavLink>
+
        <div className="collapse navbar-collapse" id="navbarSupportedContent">
          <ul className="navbar-nav ml-auto">
            <li className="nav-item">
